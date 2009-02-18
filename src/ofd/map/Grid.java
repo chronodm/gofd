513f1ad82d02b94a41dba9a479f577b830c31080
chronodm <chronodm@1fcbd0ea-f93f-11dd-8c4e-4bbb75a408b7>
2009-02-18 08:46:00 +0000

    Added list of tasks

--- src/ofd/map/Grid.java
+++ src/ofd/map/Grid.java
@@ -4,6 +4,62 @@ import java.util.*;
 
 public abstract class Grid {
 
+  /*
+   TODO __REDESIGN__: 1) rename Coord to P
+
+   TODO __REDESIGN__: 2) add: interface Range implements Iterable<Integer> {
+     int from();
+     int to();
+     int size();
+   }
+
+   TODO __REDESIGN__: 3) interface IGrid<S> { // plus AbstractGrid<S>
+     Range xRange();
+     Range yRange();
+     S get(P p);
+   }
+   
+   TODO __REDESIGN__: 4) rename Grid to MGrid (implements IGrid<MSquare>)
+   
+   TODO __REDESIGN__: 5) add VGrid interface (extends IGrid<VSquare>)
+                         (note: uses relative coordinates rather than absolute;
+                         size is always odd, ranges are -fov to +fov)
+   
+   TODO __REDESIGN__: 7) adapt MapDisplay to show POV w/ location, direction, FOV
+   
+   TODO __REDESIGN__: 8) generalize to VGrid (may include generalizing MSquare/VSquare)
+
+   TODO __REDESIGN__: 9) add POV.see(MGrid) (returns VGrid) layering:
+                         VGrid -> Rotation -> Translation/Cliption -> MGrid
+                         
+                         Translation/Cliption:
+                           Given:
+                             - an MGrid,
+                             - a (non-rotated) POV origin,
+                             - and an x and y offset
+                           Return:
+                             - the corresponding MSquare as follows (for x, y):
+
+                                // origin: POV in map coordinates
+                                // offset: view coordinate of the square we're looking for
+                                // map size: size of the map (used for wrapping)
+                                int mapCoord(int origin, int offset, int mapSize) {
+                                  int c = (origin + offset) % mapSize;
+                                  if (c >= 0) {
+                                    return c;
+                                  }
+                                  return c + mapSize;
+                                }
+
+                         Rotation:
+                           Given:
+                             - A non-rotated VGrid produced by Translation/Cliption
+                             - A POV facing direction
+                           Return:
+                             - A rotated VGrid, with each square properly rotated
+   */
+
+  
   // ////////////////////////////////////////////////////////////
   // Abstracts
 
